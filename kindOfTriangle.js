bea80deb052697e7d685bfc84dd6882c147631a5
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-14 20:26:56 +0000

    kind of Triangle

---
+++ kindOfTriangle.js
@@ -0,0 +1,13 @@
+let a= 5;
+let b= 7;
+let c= 8;
+if (a==b&& b==c && c==a) {
+    console.log("Its a Equilateral triangle")
+}
+else if(a != b && b!=c && c!=a){
+    console.log("Its a Scalene triangle")
+}
+else{
+    console.log("Its a Isosceles triangle")
+}
+console.log("End")
