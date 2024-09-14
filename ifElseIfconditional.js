11bcdb544c79639584fee3f0b0427b5ae632813e
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-14 19:11:44 +0000

    mainn

---
+++ ifElseIfconditional.js
@@ -0,0 +1,13 @@
+let isUserPremium = false;
+let isUserVip = false;
+if (isUserPremium) {
+    console.log("Enjoy the Premium Content in Hotstar")
+
+}
+else if(isUserVip) {
+    console.log("Enjoy the Vip Content in Hotstar")
+
+}
+else{
+    console.log("Please Purchase either Premium or Vip plan to enjoy the Content in Hotstar")
+}
