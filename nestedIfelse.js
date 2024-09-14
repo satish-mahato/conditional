8f78d211d868c4979ac71ed9df3cbe2c7fd77402
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-14 19:22:17 +0000

    nested

---
+++ nestedIfelse.js
@@ -0,0 +1,12 @@
+let isUserPrime= false;
+let isUserDisovery = true;
+if(isUserPrime){
+    if(isUserDisovery){
+        console.log("Enjoy Both Content")
+
+    }
+else{ console.log("Enjoy the Prime Content:")}
+}
+else{
+    console.log("Please Purchase Subcription to enjoy the Content")
+}
