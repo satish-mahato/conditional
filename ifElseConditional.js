516d47d938d76e3ab6710eea6aefb66006682c23
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-14 18:49:22 +0000

    update new

---
+++ ifElseConditional.js
@@ -0,0 +1,8 @@
+console.log("Welcome to prime video")
+let isPrimevideoMemer = true
+if(isPrimevideoMemer){
+    console.log("Enjoy the Content")
+}
+else {
+    console.log("Please buy the Member Plan")
+}
