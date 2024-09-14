c4938e292d7bb53e9843eca5ded4822d735f6431
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-14 19:59:44 +0000

    smallest numb

---
+++ smallestNum.js
@@ -0,0 +1,13 @@
+// which is the smallest number among the three numbers?
+let a=10;
+let b=20;
+let c=9;
+if(a<b && a<c){
+    console.log(a)
+}
+else if(b<a && b<c){
+    console.log(b)
+}
+else{
+    console.log(c)
+}
