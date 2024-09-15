e1a346d5f9a737101f20ad5bd9c80c07888bf987
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-15 04:58:30 +0000

    sum

---
+++ loop/EvenNuminForLoop.js
@@ -0,0 +1,5 @@
+for(let x=1;x<=25; x++){
+    if(x%2==0){
+        console.log(x);
+    }
+}
---
+++ loop/sumofEvenNuminForLoop.js
@@ -0,0 +1,8 @@
+let ans=0;
+for(let x=1;x<=25; x++){
+    if(x%2==0){
+        ans+=x;
+        
+    }
+}
+console.log(ans);
