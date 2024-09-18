870c05165112cd0c7c28a21892c9cb3326a61d6c
satishsingh202dd <satishsingh202dd@gmail.com>
2024-09-18 18:00:16 +0000

    fib print

---
+++ loop/fibPrint.js
@@ -0,0 +1,19 @@
+function printFibonacci(n){
+    if (n==0){
+        console.log(0);
+    }
+    if (n>=1){
+        console.log(0);
+        console.log(1);
+    }
+    let last =1;
+    let secondLast=0;
+    for (let i=2; i<=n;i++){
+        let ans=last+secondLast;
+        console.log(ans);
+        secondLast=last;
+        last=ans;
+    }
+    }
+
+printFibonacci(13)
